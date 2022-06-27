1c011ba9dc382317a00d6c92c876bd4e36ed4c6b
Tony Stark <tony_stark_dex@protonmail.com>
2022-06-27 16:15:09 -0500

    graph implimentation update

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,32 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Voting from '@/components/sections/Voting.vue'
-import Proposal from '@/components/sections/Proposal.vue'
-import NewProposal from '@/components/sections/NewProposal.vue'
+// import Voting from '@/components/sections/Voting.vue'
+// import Proposal from '@/components/sections/Proposal.vue'
+// import NewProposal from '@/components/sections/NewProposal.vue'
 
 export default new createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/',
-      redirect: '/dao/0/proposals',
+      // redirect: '/dao/0/proposals',
     },
-    {
-      path: '/dao/:assetId/proposals',
-      name: 'dao',
-      props: true,
-      component: Voting,
-    },
-    {
-      path: '/dao/:assetId/proposals/:proposalId',
-      name: 'proposal',
-      props: true,
-      component: Proposal,
-    },
-    {
-      path: '/dao/:assetId/proposals/create',
-      name: 'newProposal',
-      props: true,
-      component: NewProposal,
-    },
+    // {
+    //   path: '/dao/:assetId/proposals',
+    //   name: 'dao',
+    //   props: true,
+    //   component: Voting,
+    // },
+    // {
+    //   path: '/dao/:assetId/proposals/:proposalId',
+    //   name: 'proposal',
+    //   props: true,
+    //   component: Proposal,
+    // },
+    // {
+    //   path: '/dao/:assetId/proposals/create',
+    //   name: 'newProposal',
+    //   props: true,
+    //   component: NewProposal,
+    // },
   ],
 })
