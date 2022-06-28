89effa8abb53248a73513c8ef4419f3e04dbf695
Tony Stark <tony_stark_dex@protonmail.com>
2022-06-28 05:36:03 -0500

    implimentation update

---
+++ src/components/proposals/ProposalList.vue
@@ -0,0 +1,15 @@
+<template>
+        <h2 class="title is-size-3">{{ proposalStatus }}</h2>
+</template>
+
+<script>
+export default {
+    name: "ProposalList",
+    props: {
+        proposalStatus: {
+            type: String,
+            required: true,
+        }
+    }
+}
+</script>
