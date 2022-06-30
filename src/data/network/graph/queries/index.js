e19c5a5c341e1fa5f614bfe3a38c952a015ebbe0
abstrucked <mxhgraphics@gmail.com>
2022-06-30 03:39:02 +0200

    newPaperProposal + graph_fix +  minor changes

--- src/data/network/graph/queries/index.js
+++ src/data/network/graph/queries/index.js
@@ -35,6 +35,67 @@ const THREAD_PROPOSAL_QUERY = gql`
   }
 `
 
+
+
+
+const ALL_ASSETS_QUERY = gql`
+  query {
+    deployedAssets {
+      id
+      mintedAsset {
+        id
+        dataURI
+      }
+      contract
+      symbol
+      numOfShares
+      owners {
+        id
+        owner
+        shares
+      }
+      marketOrders {
+        id
+        orderType
+        price
+        amount
+      }
+      proposals {
+        id
+        creator
+        dataURI
+        votes {
+          id
+          voter
+          voteType
+          count
+        }
+        startTimestamp
+        endTimestamp
+      }
+    }
+  }
+`
+
+const ALL_ASSET_PROPOSALS_QUERY = gql`
+  query Proposals($assetId: String!) {
+    proposals(asset: $assetId) {
+      id
+      creator
+      dataURI
+      votes {
+        id
+        voter
+        voteType
+        count
+      }
+      startTimestamp
+      endTimestamp
+    }
+  }
+`
 export {
-  THREAD_PROPOSAL_QUERY
+  THREAD_PROPOSAL_QUERY,
+  ALL_ASSET_PROPOSALS_QUERY,
+  ALL_ASSETS_QUERY
 }
