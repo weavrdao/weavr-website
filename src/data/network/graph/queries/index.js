98f1e8fa653ff21983cf3600bd495853c058e8f0
abstrucked <mxhgraphics@gmail.com>
2022-07-12 16:56:37 +0200

    ALL_ASSETS_QUERY update

--- src/data/network/graph/queries/index.js
+++ src/data/network/graph/queries/index.js
@@ -39,63 +39,631 @@ const THREAD_PROPOSAL_QUERY = gql`
 
 
 const ALL_ASSETS_QUERY = gql`
-  query {
-    deployedAssets {
+query frabrics
+  {
+    frabrics {
       id
-      mintedAsset {
+      paperProposals {
         id
-        dataURI
+        baseProposal {
+          info
+        }
       }
-      contract
-      symbol
-      numOfShares
-      owners {
-        id
-        owner
-        shares
+      token {
+        name
+        symbol
+        decimals
+        supply
+        tradeToken
+        globalAcceptance
+        whitelist {
+          id
+          person
+          kycHash
+          removed
+        }
+        freezelist {
+          id
+          person
+          frozenUntil
+        }
+        orderBook {
+          id
+          price
+          type
+          totalAmount
+        }
+        executedOrders {
+          id
+          blockTimestamp
+          orderer
+          executor
+          price
+          amount
+        }
+        balances {
+          id
+          holder {
+            id
+          }
+          amount
+          transfersFrom {
+            timestamp
+            to {
+              id
+            }
+            amount
+          }
+        }
       }
-      marketOrders {
+      participants {
         id
-        orderType
+        address
+        type
+      }
+      governorStatuses {
+        id
+        address
+        status
+      }
+      threads {
+        id
+        contract
+        variant
+        governor
+        erc20 {
+          name
+          symbol
+          decimals
+          supply
+          tradeToken
+          globalAcceptance
+          whitelist {
+            id
+            person
+            kycHash
+            removed
+          }
+          freezelist {
+            id
+            person
+            frozenUntil
+          }
+          orderBook {
+            id
+            price
+            type
+            totalAmount
+          }
+          executedOrders {
+            id
+            blockTimestamp
+            orderer
+            executor
+            price
+            amount
+          }
+          balances {
+            id
+            holder {
+              id
+            }
+            amount
+            transfersFrom {
+              timestamp
+              to {
+                id
+              }
+              amount
+            }
+          }
+        }
+        descriptor
+        descriptorChangeProposals {
+          id
+          descriptor
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        upgradeProposals {
+          id
+          frabric {
+            id
+          }
+          thread {
+            id
+          }
+          beacon
+          instance
+          version
+          code
+          data
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        tokenActionProposals {
+          id
+          frabric {
+            id
+          }
+          thread {
+            id
+          }
+          token
+          target
+          mint
+          price
+          amount
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        participantRemovalProposals {
+          id
+          frabric {
+            id
+          }
+          thread {
+            id
+          }
+          participant
+          removalFee
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        frabricChangeProposals {
+          id
+          frabric
+          thread {
+            id
+          }
+          governor
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        governorChangeProposals {
+          id
+          thread {
+            id
+          }
+          governor
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        ecosystemLeaveProposals {
+          id
+          frabric
+          thread {
+            id
+          }
+          governor
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+        dissolutionProposals {
+          id
+          thread {
+            id
+          }
+          token
+          price
+          baseProposal {
+            id
+            thread {
+              id
+            }
+            frabric {
+              id
+            }
+            creator
+            type
+            state
+            votes {
+              id
+              voter
+              voteDirection
+              count
+            }
+            info
+          }
+        }
+      }
+      threadProposals {
+        id
+        governor
+        name
+        symbol
+        descriptor
+        data
+        baseProposal {
+          id
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
+        }
+      }
+      participantProposals {
+        id
+        proposer
+        participant
+        baseProposal {
+          id
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
+        }
+      }
+      upgradeProposals {
+        id
+        frabric {
+          id
+        }
+        thread {
+          id
+        }
+        beacon
+        instance
+        version
+        code
+        data
+        baseProposal {
+          id
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
+        }
+      }
+      tokenActionProposals {
+        id
+        frabric {
+          id
+        }
+        thread {
+          id
+        }
+        token
+        target
+        mint
         price
         amount
-      }
-      proposals {
-        id
-        creator
-        dataURI
-        votes {
+        baseProposal {
           id
-          voter
-          voteType
-          count
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
         }
-        startTimestamp
-        endTimestamp
       }
-    }
-  }
-`
-
-const ALL_ASSET_PROPOSALS_QUERY = gql`
-  query Proposals($assetId: String!) {
-    proposals(asset: $assetId) {
-      id
-      creator
-      dataURI
-      votes {
+      participantRemovalProposals {
         id
-        voter
-        voteType
-        count
+        frabric {
+          id
+        }
+        thread {
+          id
+        }
+        participant
+        removalFee
+        baseProposal {
+          id
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
+        }
       }
-      startTimestamp
-      endTimestamp
+      bondRemovalProposals {
+        id
+        frabric {
+          id
+        }
+        participant
+        slash
+        amount
+        baseProposal {
+          id
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
+        }
+      }
+      threadProposalProposals {
+        id
+        frabric {
+          id
+        }
+        thread
+        info
+        baseProposal {
+          id
+          thread {
+            id
+          }
+          frabric {
+            id
+          }
+          creator
+          type
+          state
+          votes {
+            id
+            voter
+            voteDirection
+            count
+          }
+          info
+        }
+      }
+      vouchers {
+        id
+        signer
+        participant
+      }
     }
-  }
+    bonds {
+      id
+      holdings {
+        id
+        governor
+        amount
+      }
+    }
+    crowdfunds {
+      id
+      state
+      amountDeposited
+      target
+      thread {
+        id
+      }
+      erc20 {
+        id
+      }
+      deposits {
+        id
+        depositor
+        amount
+      }
+      withdrawals {
+        id
+        depositor
+        amount
+      }
+      distributions {
+        id
+        distribution {
+          token
+          amount
+          claims {
+            id
+            person
+            amount
+          }
+        }
+      }
+    }
+    auctions {
+      id
+      seller
+      token
+      traded
+      totalAmount
+      start
+      length
+      batches {
+        id
+        bids {
+          id
+          bidder
+          amount
+        }
+        isComplete
+      }
+    }
+    timelocks {
+      id
+      timestamp
+      token
+      months
+      amountClaimed
+    }
+  }  
 `
 export {
-  THREAD_PROPOSAL_QUERY,
-  ALL_ASSET_PROPOSALS_QUERY,
+  
   ALL_ASSETS_QUERY
 }
