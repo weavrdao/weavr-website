6af2af8ba5bf94a39047fc35e0daf0319817031a
Tony Stark <tony_stark_dex@protonmail.com>
2022-08-05 10:06:45 -0500

    baseProposal mod w/ vue-mc

--- src/models/proposals/baseProposal.js
+++ src/models/proposals/baseProposal.js
@@ -1,40 +1,41 @@
-/* eslint-disable class-methods-use-this */
-import { Model } from "vue-mc"
-import { ProposalTypes } from "../common";
-/**
- * Proposal model.
- * @property {string} id ID of the proposal
- * @property {string} creatorAddress Address of the proposal creator
- * @property {number} startTimestamp Unix timestamp marking the start of the voting window
- * @property {number} endTimestamp Unix timestamp marking the end of the voting window
- * @property {Vote[]} votes Votes posted on the proposal
- * @property {bool} supermajority signals if the supermagiority is required or not
- * @property {string} state Current state of the proposal
- * @property {string} info ipfs hash of proposal information
- * @property {ProposalTypes} type Type of proposal this is
- */
-class BaseProposal extends Model{
-  defaults() {
-    return {
-      id: null,
-      creatorAddress: null,
-      startTimestamp: null,
-      endTimestamp: null,
-      votes: null,
-      supermajority: null,
-      state: "",
-      info: "",
-      type: "",
-    }
-  }
+// /**
+//  * Proposal model.
+//  * @property {string} id ID of the proposal
+//  * @property {string} creatorAddress Address of the proposal creator
+//  * @property {number} startTimestamp Unix timestamp marking the start of the voting window
+//  * @property {number} endTimestamp Unix timestamp marking the end of the voting window
+//  * @property {Vote[]} votes Votes posted on the proposal
+//  * @property {bool} supermajority signals if the supermagiority is required or not
+//  */
+// /*
+// id: ID!
+// thread: Thread
+// frabric: Frabric
+// creator: Bytes!
+// type: Int!
+// state: ProposalState!
+// votes: [Vote!]!
+// info: Bytes!
+// supermajority: Boolean!
+// startTimestamp: Int!
+// endTimestamp: Int!
+// */
+// class BaseProposal {
+//   constructor(
+//     id,
+//     creatorAddress,
+//     startTimestamp,
+//     endTimestamp,
+//     votes,
+//     supermajority
+//   ) {
+//     this.id = id
+//     this.creatorAddress = creatorAddress
+//     this.startTimestamp = startTimestamp
+//     this.endTimestamp = endTimestamp
+//     this.votes = votes
+//     this.supermajority = supermajority
+//   }
+// }
 
-  methods() {
-    return {
-      fetch() {
-
-      }
-    }
-  }
-}
-
-export default { BaseProposal }
+// export default { BaseProposal }
