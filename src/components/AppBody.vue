d78ff72d6325741a2dcceb9d6a3af3dd0dbaa940
bill0x2a <billysmith1998@live.com>
2022-08-21 18:41:09 +0100

    Add header and footer

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <HeaderNavigationBar />
-    <RouterView class="container p-5" /> 
+    <RouterView class="container p-5" />
+    <Footer />
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import HeaderNavigationBar from "./layout/navigation/HeaderNavigationBar.vue";
-import ProposalListVue from "./proposals/ProposalList.vue";
+import Footer from "./layout/navigation/Footer.vue";
 
 export default {
   name: "AppBody",
   components: {
     HeaderNavigationBar,
+    Footer,
   },
   computed: {
     ...mapGetters({
