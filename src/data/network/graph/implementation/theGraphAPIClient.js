c3203e7967821a7279999675cb5cf3319697d855
Abstrucked <mxhgraphics@gmail.com>
2022-08-24 22:40:32 +0200

    pointed subgraph to Goerli latest deployment 24/08

--- src/data/network/graph/implementation/theGraphAPIClient.js
+++ src/data/network/graph/implementation/theGraphAPIClient.js
@@ -5,7 +5,7 @@ import {
 import { GraphQLAPIClient } from "../graphQLAPIClient"
 
 const client = new ApolloClient({
-  uri: "https://api.studio.thegraph.com/query/33141/weavr-test/v0.0.5",
+  uri: "https://api.thegraph.com/subgraphs/name/abstrucked/weavr_goerli",
   cache: new InMemoryCache()
 })
 
