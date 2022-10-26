8e269e1238cf383145be853688ef2133962857c7
zeryx <1892175+zeryx@users.noreply.github.com>
2022-10-26 02:05:16 -0400

    added a privacy page

---
+++ src/components/pages/PrivacyPage.vue
@@ -0,0 +1,91 @@
+<template>
+ <body>
+ PRIVACY POLICY WEAVR DAO
+
+ This privacy policy (“Privacy Policy”) provides extensive insight into how WeavrDAO (“Weavr”, “we”, “us”, “our”), operated by 12321 DAO LLC, and platforms affiliated with Weavr, including but not limited to the Websites [.], its Application [.], and Smart Contracts [.], (collectively “Platforms”) collect, share or use data about you (“user(s)”, “you”, “your”) with connection to the Platforms.
+ This Privacy Policy, together with our Terms of Use (“Terms”) [.] govern the collection and processing of your data and is applicable to all the information collected through the Platforms, upon the use of such Platforms, any related services, marketing, events and/or any other locations that are in any way linked to this Privacy Policy (collectively, the “Services”).
+ Please read this Policy and our Terms carefully. By accessing our Platforms, you are consenting to the information collection and use practices described in this Privacy Policy. If you do not agree to this Privacy Policy, you should immediately cease to continue the use of the Services.
+
+ COLLECTION OF INFORMATION
+ Our servers may automatically log standard data provided by your browser upon the use of our Services. Such information may include but is not limited to, browser type and version, Internet Protocol (IP) address, geolocation data, username, email, and/or virtual asset wallet addresses.
+ Other information about you that is publicly available or provided to us by third parties may be combined with automatically-collected information. Such information may include but is not limited to, the transaction history of digital assets.
+ Personal information such as name, email, or wallet address may be collected when you communicate with us regarding issues, including but not limited to, support, reporting errors, and/or reporting bugs.
+ Your consent to use of your personal information may be required in certain circumstances and the same may be withdrawn at any time, by reaching out to us via the contact details provided in this Privacy Policy. We shall get in touch with you whenever consent to process any data is required.
+ USE OF INFORMATION
+ Use of the collected information may be for the following purposes -
+ providing services to you;
+ complying with regulatory and legal obligations;
+ exercising or defending any legal rights;
+ communicating with you or managing any request;
+ for legitimate business interests, such as business research and analysis, managing our Platforms and Services;
+ monitoring the use of Platforms for business purposes which may include analysis of usage, measurement of site performance, and generation of marketing reports;
+ inquiring into any complaints or queries;
+ preventing and responding to actual or potential fraud or illegal activities;
+ operating our Platforms, customer support, marketing, and research services related to the Platforms.
+ We do not anticipate the use of your information apart from business purposes. Such purposes include, but are not limited to -
+ Performing services requested by you such as responding to your comments, questions, and requests, and providing information support; operating and managing the Services; detecting, preventing, and addressing fraud, breach of Terms, and threats, or harm; sending you technical notices, updates, security alerts, information regarding changes to our policies, and support and administrative messages; and compliance with legal and regulatory requirements.
+ Protecting the security and integrity of the Services; improving the Services; and/or fulfilling any other business purpose, with your consent.
+ Notwithstanding the above, we may use information that does not identify you (including information that has been anonymized, aggregated, or de-identified) for any purpose except as prohibited by applicable law.
+ SHARING OF INFORMATION
+ We may share information with related parties including but not limited to, the following -
+ We may share information with related entities and/or our affiliates, including but not limited to when they act as our service providers.
+ We may share your information with you at your request or direction.
+ We may share information during negotiations of, in connection with, any proposed or actual merger, purchase, sale, or other similar acquisition or business combination.
+ We may share information for audits and legal compliance with our professional advisors.
+ Information stored by us may be shared with public officials for legal compliance enforcement and authorized security concerns.
+ We may share information with third-party service providers for business purposes, including fraud detection and prevention, security threat detection, technology, payment processing, customer support, data analytics, information storage, and transaction monitoring. All service providers we engage are restricted to only utilizing the information on our behalf and following our instructions.
+ Notwithstanding the above, we may share information that, includes but is not limited to information that has been aggregated or any such information that does not identify you (de-identified), except as prohibited by applicable law.
+ CHILDREN’S PRIVACY
+ We do not collect personal information of children under the age of thirteen (13) and our Services are not aimed directly at children under the age of thirteen (13).
+ If we are made aware of use by children below thirteen (13), by you or anyone similar using the Services, we shall make commercially reasonable efforts to delete such information from our database within 90 days of such notice.
+ If you are a parent and/or guardian of a minor under the age of thirteen (13) and are made aware of personal data that may have been provided to us, without verification of parental consent, please contact us immediately. We shall make commercially reasonable efforts to delete such information from our database within 90 days of such notice.
+ CHANGE OF SETTINGS
+ You may control how your devices permit the use of cookies. If you so choose, you may block or delete our cookies from your browse.
+ You may be able to configure your browser settings to use our Services without some cookie functionality. You can delete cookies manually or set your browser to delete cookies after a determined period automatically.
+ SECURITY OF INFORMATION
+ We shall ensure that your personal information collected by us is subject to appropriate industry-standard security measures. The nature of internet or electronic transmission, however, cannot be completely risk-free thus we cannot guarantee the absolute security of the same.
+ We shall not be responsible for any interruption or interception of any communications through the internet or for changes to or losses of data. You are responsible for maintaining the security of any password, user ID or any other form of authentication involved in obtaining access to secure areas of any of our Services.
+ We only store your information for a limited period of time and strictly only for as long as is necessary for the relevant purpose and/or for the duration necessary to comply with laws, rules, legal obligations, and/or regulations.
+ EUROPEAN UNION/ UNITED KINGDOM ADDENDUM
+ Storing of data -
+ The personal information collected by us may be transferred to and processed in countries outside the European Economic Area (“EEA”) and the United Kingdom (“UK”). We shall take the necessary steps to ensure that your personal information is treated securely and in accordance with this Privacy Policy.
+ For any transfers of data outside the EEA or the UK, the data transfer shall be under the European Commission’s model contracts for the transfer of personal information to third countries (i.e., the standard contractual clauses) (the “Model Clauses”), or any equivalent contracts issued by the relevant competent authority of the UK.
+ Data Protection Rights -
+ You are entitled to a certain number of legal rights in relation to the personal information that we may hold about you. These rights can be exercised at any time by contacting us.
+ The following rights apply regarding the collection of your personal information:
+ Right to access: you have the right to access your personal information stored with us, how we use it, and who we share it with.
+ Right to erasure: In certain circumstances, you have the right to delete your personal information stored with us. There may be circumstances where you ask us to erase personal information, but we are legally entitled to retain it; in such circumstances, the same shall not be erased.
+ Right to objection: you have the right to object to our processing of your personal information. There may be circumstances where you object to or asks us to restrict the processing of personal information, but we are legally entitled to refuse that request.
+ Right of portability: you have the right to receive a copy of all your personal information stored by us in a structured, commonly used, and machine-readable format and/or request transfer of the same to a third party, in certain circumstances, and with certain exceptions.
+ Right to rectification: you have the right to correct any inaccurate personal information.
+ Right to restrict processing: you have the right to require us to stop processing the personal information we hold about you, other than for storage purposes, in certain circumstances.
+ Withdrawal of consent: Where we rely on consent to process your personal information, you have the right to withdraw this consent at any time In certain circumstances it may be lawful for us to continue processing without consent if we have a legitimate reason (other than consent) for doing so.
+ The aforementioned rights are not absolute and may be limited by law.
+ Requests and Complaints -
+ If you have complaints about how we process your personal information, please contact us at [.] and we shall respond to your request as soon as possible.
+ If you think an infringement of your data protection rights has occurred, you may file a claim with the data protection supervisory authority in the EEA country of your residence.
+ ADDITIONAL DISCLOSURES UNDER CALIFORNIA PRIVACY LAW
+ Under the California Consumer Privacy Act of 2018 (“CCPA”), California residents are afforded certain rights to know, delete and opt out. It additionally also requires notices to be provided and means to exercise consumer rights by businesses collecting and/or disclosing personal information.
+ As a resident of California, you may have the following rights in relation to the collection of your information. However, these rights are not absolute and in certain cases, a request that is not permitted by law may be declined by us.
+ Request related to use and collection of information: you may request information about how we have collected and used your Personal Information during the past 12 months.
+ Access: you may  request a copy of the Personal Information we collected about you during the past 12 months.
+ Deletion: you may request that we delete your personal information.
+ Opt Out: you have the right to restrict (opt-out) the sharing of your personal information with third parties.
+ RIGHTS RESERVED
+ Weavr reserves the right to modify, update and/or change the Privacy Policy contained herein from time to time. Such changes shall be reflected in the Privacy Policy of Weavr and continuing usage shall imply acceptance of the new Privacy Policy.
+
+ CONTACT US
+ For further information on the collection, use, disclosure, transfer, or processing of personal information or the exercise of any of the rights listed above, please contact Weavr by sending an email on [.] or contacting us on [.]
+
+ </body>
+</template>
+
+<script>
+export default {
+  name: "PrivacyPage.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
