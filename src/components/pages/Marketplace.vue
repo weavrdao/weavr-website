cb8e2f0c4d2b8bf069ab85c9fbb7385630321f4d
abstrucked <mxhgraphics@gmail.com>
2022-11-16 12:58:50 +0100

    marketplace wrapper component

---
+++ src/components/pages/Marketplace.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="notification has-background-mediumBlue">THIS IS IT</div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+    name: "Marketplace",
+}
+</script>
