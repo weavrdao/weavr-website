baab74d4bafe730ba2f96d8ca1d934016a3e5ffd
abstrucked <mxhgraphics@gmail.com>
2022-11-23 00:31:08 +0100

    fixed redirect from whitelist page and button naming

--- src/whitelist/page/Whitelist.vue
+++ src/whitelist/page/Whitelist.vue
@@ -8,7 +8,7 @@
       <h3 v-if="!address">Please connect your wallet to continue</h3 >
       <button class="button tag is-primary has-radius-xs is-large is-clickable " v-if="!address" @click="openConnectWalletPage">Connect Wallet</button>
       <button class="button tag is-primary has-radius-xs is-large is-clickable mt-3" v-if="!address" @click="openLoginModal">Login as Guest</button>
-      <button class="button tag is-primary has-radius-xs is-large is-clickable " v-else-if="whitelisted" @click="continueToHomepage">Continue to Homepage</button>
+      <button class="button tag is-primary has-radius-xs is-large is-clickable " v-else-if="whitelisted" @click="continueToGovernance">Continue to Governance</button>
       <div class="flex-center" v-else>
         <h3 class="has-text-centered">We're sorry, you have not yet been whitelisted</h3>
         <p class="has-text-centered mb-3">Check the account selected in your wallet if you believe this is not the case</p>
@@ -44,8 +44,8 @@ export default {
     openConnectWalletPage () {
       this.$router.push("/walletConnect")
     },
-    continueToHomepage () {
-      this.$router.push({path: "/weavr"});
+    continueToGovernance () {
+      this.$router.push({path: "/"+CONTRACTS.WEAVR});
     },
     openLoginModal () {
       this.$router.push("/login")
