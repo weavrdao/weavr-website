711ad7f1dfa4305e680552e81902b53be873e304
Tony Stark <afemiadom@icloud.com>
2023-01-13 09:47:23 -0600

    checked menu bar focus color

--- src/components/layout/navigation/HeaderNavigationBar.vue
+++ src/components/layout/navigation/HeaderNavigationBar.vue
@@ -1,46 +1,53 @@
 <template>
-  <nav class="navbar  p-5" role="navigation" aria-label="main navigation">
-    <div class="container ">
+  <nav class="navbar p-5" role="navigation" aria-label="main navigation">
+    <div class="container">
       <div class="navbar-brand">
-      <a class="navbar-item" @click="transitTo('/')">
-        <div class="title brand has-text-white is-flex is-align-items-center" >
-          <div class="image p-2"><img class="mx-2" src="../../../assets/logo/new-logo.svg" alt=""></div>
-          Weavr</div>
-      </a>
-      <a
-        role="button"
-        ref="menuButton"
-        :class="[
-          navigation.isOpen ? 'is-active' : '',
-          'navbar-burger',
-          'has-border-bottom',
-        ]"
-        @click="menuToggle()"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="frabric-navbar"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div :class="[navigation.isOpen ? 'is-active' : '', 'navbar-menu']">
-      <div class="navbar-start">
+        <a class="navbar-item" @click="transitTo('/')">
+          <div class="title brand has-text-white is-flex is-align-items-center">
+            <div class="image p-2">
+              <img
+                class="mx-2"
+                src="../../../assets/logo/new-logo.svg"
+                alt=""
+              />
+            </div>
+            Weavr
+          </div>
+        </a>
         <a
+          role="button"
+          ref="menuButton"
           :class="[
-            isItemCurrent(item) ? 'active-link' : '',
-            'navbar-item navlink',
-            '',
-            'p-3 mt-2 is-primary'
+            navigation.isOpen ? 'is-active' : '',
+            'navbar-burger',
+            'has-border-bottom',
           ]"
-          v-for="item in navigation.items"
-          :key="item.name"
-          v-on:click="transitTo(item.route)"
+          @click="menuToggle()"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="frabric-navbar"
         >
-          <div class="">{{ item.name }}</div>
-          <!-- <div v-else class="">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div :class="[navigation.isOpen ? 'is-active' : '', 'navbar-menu']">
+        <div class="navbar-start">
+          <a
+            :class="[
+              isItemCurrent(item) ? 'active-link' : '',
+              'navbar-item navlink',
+              '',
+              'p-3 mt-2 is-primary',
+            ]"
+            v-for="item in navigation.items"
+            :key="item.name"
+            v-on:click="transitTo(item.route)"
+          >
+            <div class="">{{ item.name }}</div>
+            <!-- <div v-else class="">
             <div class="dropdown is-hoverable is-right">
               <div class="dropdown-trigger">
                 {{item.name}}
@@ -59,28 +66,32 @@
               </div>
             </div>
           </div> -->
-          <span v-if="item.icon">
-            <unicon width="15" height="15" :name="item.icon" fill="gray"></unicon>
-          </span>
-        </a>
-      </div>
+            <span v-if="item.icon">
+              <unicon
+                width="15"
+                height="15"
+                :name="item.icon"
+                fill="gray"
+              ></unicon>
+            </span>
+          </a>
+        </div>
 
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <SignerAddress />
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <SignerAddress />
+          </div>
         </div>
       </div>
     </div>
-    </div>
-    
   </nav>
 </template>
 
 <script>
-import SignerAddress from '../../views/address/SignerAddress.vue'
-import { useRoute } from "vue-router"
-import { mapGetters, mapActions } from "vuex"
-import { CONTRACTS } from '../../../services/constants'
+import SignerAddress from "../../views/address/SignerAddress.vue";
+import { useRoute } from "vue-router";
+import { mapGetters, mapActions } from "vuex";
+import { CONTRACTS } from "../../../services/constants";
 export default {
   name: "HeaderHavigationBar",
   components: {
@@ -91,26 +102,30 @@ export default {
       navigation: {
         isOpen: false,
         items: [
-          { name: "Governance", route: `/dao/${CONTRACTS.WEAVR}`},
-          { 
-            name: "Marketplace", 
-            route: "/marketplace", 
+          { name: "Governance", route: `/dao/${CONTRACTS.WEAVR}` },
+          {
+            name: "Marketplace",
+            route: "/marketplace",
             childs: [
-              { 
-                name: "Needles", 
-                route: { path: "marketplace", params: { market: "needle"}}
+              {
+                name: "Needles",
+                route: { path: "marketplace", params: { market: "needle" } },
               },
-              { 
-                name: "Threads", 
-                route: { path: "/marketplace/threads"}
-              }
-            ]
+              {
+                name: "Threads",
+                route: { path: "/marketplace/threads" },
+              },
+            ],
           },
-          { name: "Resolutions", route: "/resolutions", icon: "arrow-up-right"},
-          { name: "FAQ", route: "/faq", icon: "arrow-up-right" }
+          {
+            name: "Resolutions",
+            route: "/resolutions",
+            icon: "arrow-up-right",
+          },
+          { name: "FAQ", route: "/faq", icon: "arrow-up-right" },
         ],
       },
-    }
+    };
   },
   computed: {
     ...mapGetters(["currentNavigationItem"]),
@@ -118,21 +133,21 @@ export default {
   methods: {
     ...mapActions(["goBack"]),
     isItemCurrent(item) {
-      return useRoute().fullPath.includes(item.route)
+      return useRoute().fullPath.includes(item.route);
     },
     transitTo(path) {
-      this.$router.push(path)      
+      this.$router.push(path);
       console.log(this.$route.name);
       this.navigation.isOpen ? this.menuToggle() : null;
     },
     menuToggle() {
-      this.navigation.isOpen = !this.navigation.isOpen
+      this.navigation.isOpen = !this.navigation.isOpen;
     },
     print(child) {
-      console.log(child)
-    }
+      console.log(child);
+    },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -163,7 +178,7 @@ export default {
 }
 
 .active-link {
-  background: linear-gradient(to top,  rgba(255,255,255,0) 20%,$mediumBlue 100%);
+  color: $mediumBlue;
 }
 
 .dropdown-content {
