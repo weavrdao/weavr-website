27d8afb731ed95774a8283f207bb04c37a08cb56
abstrucked <mxhgraphics@gmail.com>
2023-01-18 12:26:17 +0100

    Styling

--- docs/.vitepress/theme/Card.vue
+++ docs/.vitepress/theme/Card.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="columns">
-    <div class="columns is-half">
-      <img class="card-image" :src="author.avatar" alt="..."/>
+  <div class="card card-transparent">
+    <div class="card-image">
+      <figure class="image is-square">
+        <img class="" :src="author.avatar" alt="..."/>
+      </figure>
+      
     </div>
-    <div class="columns is-half">
-      <p class="card-header">Written by {{ author.name }}</p>
+    <div class="card-header ">
+      <p class="card-header-title">Written by {{ author.name }}</p>
+    </div>
+    <div class="card-content">
       <VueMarkdown :source="author.bio"/>
     </div>
   </div>
@@ -16,3 +21,14 @@ import VueMarkdown from "vue-markdown-render";
 
 const props = defineProps(["author"])
 </script>
+
+<style lang="scss">
+@import "../../../src/styles/weavr-custom.scss";
+.card-transparent {
+  background: transparent !important;
+}
+:root {
+  --vp-c-doc: #646cff;
+  --vp-c-brand-light: #747bff;
+}
+</style>
