bb2215bdf351cd618531e32e737bf7d96eb2c9a5
abstrucked <mxhgraphics@gmail.com>
2023-02-06 22:33:15 +0100

    fixed verification bug

--- src/sumsub/index.js
+++ src/sumsub/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const headers = { 
 }
 const netlify = ".netlify/functions/api"
-const env = "dev"
+const env = "prod"
 export const getAccessToken = async (id) => {
   const ID = id.slice(2)
   const url = `https://api.weavr.org/${netlify}/${env}/${ID}`
