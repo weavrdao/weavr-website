95fa2deb8a1e097c6a4def55a0ad56e390aaade0
abstrucked <mxhgraphics@gmail.com>
2023-02-09 17:00:13 +0100

    adding ThreadGovernance component

---
+++ src/components/views/market/ThreadGovernance.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    
+    <NewProposalSelector></NewProposalSelector>
+  </div>
+</template>
+
+<script>
+// import ProposalList from "@/components/proposals/ProposalList"
+import NewProposalSelector from "@/components/sections/NewProposalSelector"
+import { mapGetters } from 'vuex'
+export default {
+  name: "ThreadGovernance",
+  components: {
+    // ProposalList
+    NewProposalSelector
+  },
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    ...mapGetters({
+      threads: "threadById",
+
+    }),
+    thread() {
+      return this.threads.get(this.$route.params.threadId)
+    },
+  },
+}
+</script>
