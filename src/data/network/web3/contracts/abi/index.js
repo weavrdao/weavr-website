c4f9f0a28f4b8b354139c39c8f47e43d46e89e43
abstrucked <mxhgraphics@gmail.com>
2023-03-15 16:10:59 +0100

    adding moved files

---
+++ src/data/network/web3/contracts/abi/index.js
@@ -0,0 +1,5 @@
+import Crowdfund from "./Crowdfund.json"
+import ThreadDeployer from "./ThreadDeployer.json"
+import FrabricERC20 from "./FrabricERC20.json"
+import WEAVR from "./Frabric.json"
+export { Crowdfund, ThreadDeployer, FrabricERC20, WEAVR }
---
+++ src/models/proposals/TokenActionProposal.js
@@ -0,0 +1,26 @@
+import {BaseProposal} from "./BaseProposal";
+import {VoteType} from "@/models/vote";
+
+export class TokenActionProposal extends BaseProposal {
+  constructor(baseProposal, payload) {
+    super(baseProposal.id, baseProposal.creator, baseProposal.info, baseProposal.superMajority,
+      baseProposal.startTimestamp, baseProposal.status, baseProposal.type, baseProposal.votes)
+    const {token, target, mint, price, amount} = payload
+    this.token = token
+    this.target = target
+  this.mint = mint
+  this.price = price
+  this.amount = amount
+  }
+
+  addVote(vote) {
+    if(vote.voteDirection === 0) {
+      vote.voteDirection = VoteType.Abstain
+    } else if (vote.voteDirection === 1) {
+      vote.voteDirection = VoteType.Yes
+    } else  {
+      vote.voteDirection = VoteType.No
+    }
+    this.votes.push(vote)
+  }
+}
